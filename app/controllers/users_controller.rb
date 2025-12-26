087ed97b7bc8b49bccc34544e9b280befd7f07d5
としき <superstar101066@gmail.com>
2025-12-26 17:39:55 +0900

    ログインしていない時に詳細ページが見れる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # 追記：ログインしていなくても、showアクション（詳細ページ）は見れるように許可する
+  before_action :authenticate_user!, except: [:show]
+
   def show
     # URLの末尾にあるID（/users/1 など）を元に、特定のユーザーを取得する
     @user = User.find(params[:id])
